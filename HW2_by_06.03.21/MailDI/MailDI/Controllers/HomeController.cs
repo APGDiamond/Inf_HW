71b63097a3930442692c6a2faad77db3dea37ad1
APGDiamond <shalmaz1733@gmail.com>
2021-03-08 12:44:57 +0300

    Mail Dependency Injection 1.0

---
+++ HW2_by_06.03.21/MailDI/MailDI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailDI.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly IEmailInterface _emailService;
+        public HomeController(IEmailInterface emailService)
+        {
+            _emailService = emailService;
+        }
+        [Route("sendmessage")]
+        public async Task<IActionResult> SendMessage()
+        {
+            if (ModelState.IsValid)
+            {
+                await _emailService.SendEmailAsync("almaz1733@yandex.ru", "Тема письма", "Тест письма: тест!");
+                ViewBag.Succes = true;
+            }
+            return RedirectToAction("get");
+        }
+        [HttpGet]
+        public string Get()
+        {
+            return "Письмо отправлено";
+        }
+    }
+}
---
+++ HW2_by_06.03.21/MailDI/MailDI/Services/EmailService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace MailDI
+{
+    public class EmailService : IEmailInterface
+    {
+        private readonly EmailConfig ec;
+        public EmailService(IOptions<EmailConfig> emailConfig)
+        {
+            ec = emailConfig.Value;
+        }
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            try
+            {
+                var emailMessage = new MimeMessage();
+
+                emailMessage.From.Add(new MailboxAddress(ec.Username, ec.From));
+                emailMessage.To.Add(new MailboxAddress("", email));
+                emailMessage.Subject = subject;
+                emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+                {
+                    Text = message
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    await client.ConnectAsync(ec.Host, ec.Port, ec.UseSSL);
+                    await client.AuthenticateAsync(ec.From, ec.Password);
+                    await client.SendAsync(emailMessage);
+                    await client.DisconnectAsync(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
