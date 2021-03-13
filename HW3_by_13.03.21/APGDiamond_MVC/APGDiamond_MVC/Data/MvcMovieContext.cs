bc8b15b493df9906b2b7fcc9ac55c5febfe2c93d
APGDiamond <shalmaz1733@gmail.com>
2021-03-13 14:11:38 +0300

    MVCMovie

---
+++ HW3_by_13.03.21/APGDiamond_MVC/APGDiamond_MVC/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APGDiamond_MVC.Models;
+
+namespace APGDiamond_MVC.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ HW3_by_13.03.21/APGDiamond_MVC/APGDiamond_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace APGDiamond_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
